41dfb1b31f5d8139b55b96550a20bacaf4d05fe6
Alex Mit <alex.mit.672@gmail.com>
2026-02-15 18:24:02 +0300

    feat(storage): add storage config

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -6,6 +6,11 @@ import (
 	"github.com/d1agnozzz/url-shortener/internal/urlsanitizer"
 )
 
+type StorageConfig struct {
+	maxCollisions int
+	aliaser       aliaser.Aliaser
+}
+
 type Storage interface {
 	CreateURLMapping(url urlsanitizer.SanitizedURL) (types.URLMapping, error)
 	GetByAlias(alias aliaser.Alias) (types.URLMapping, error)
